ba80d706af081ae47681352ecb9cd7a36dd89326
jordansrowles <jordansrowles@gmail.com>
2026-07-02 19:41:06 +0100

    SonarQube: Reduce duplicate code and use constants for strings (2 adaptability issues)

--- src/devops/Rowles.LeanCorpus.Benchmarks/Search/CombinedFieldsQueryBenchmarks.cs
+++ src/devops/Rowles.LeanCorpus.Benchmarks/Search/CombinedFieldsQueryBenchmarks.cs
@@ -2,7 +2,6 @@ using BenchmarkDotNet.Attributes;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Index;
 using Lucene.Net.Search;
-using Lucene.Net.Store;
 using Lucene.Net.Util;
 using IODirectory = System.IO.Directory;
 using LeanDocument = Rowles.LeanCorpus.Document.LeanDocument;
@@ -35,6 +34,10 @@ namespace Rowles.LeanCorpus.Benchmarks;
 public class CombinedFieldsQueryBenchmarks
 {
     private const int TopN = 25;
+    private const string QueryTerm1 = "government";
+    private const string QueryTerm2 = "market";
+    private const string FieldTitle = "title";
+    private const string FieldBody = "body";
 
     public static IEnumerable<int> DocCounts => BenchmarkData.GetDocCounts(BenchmarkData.DefaultDocCount);
 
@@ -80,10 +83,10 @@ public class CombinedFieldsQueryBenchmarks
     public int LeanCorpus_CombinedFieldsQuery()
     {
         var q = new CombinedFieldsQuery(
-            ["title", "body"],
-            ["government", "market"],
+            [FieldTitle, FieldBody],
+            [QueryTerm1, QueryTerm2],
             minimumShouldMatch: MinimumShouldMatch,
-            fieldWeights: new Dictionary<string, float> { ["title"] = 2.0f, ["body"] = 1.0f });
+            fieldWeights: new Dictionary<string, float> { [FieldTitle] = 2.0f, [FieldBody] = 1.0f });
         return _leanSearcher!.Search(q, TopN).TotalHits;
     }
 
@@ -92,10 +95,10 @@ public class CombinedFieldsQueryBenchmarks
     public int LeanCorpus_BooleanQuery_MultiField()
     {
         var builder = new Rowles.LeanCorpus.Search.Queries.BooleanQuery.Builder();
-        builder.Add(new TermQuery("title", "government"), Rowles.LeanCorpus.Search.Occur.Should);
-        builder.Add(new TermQuery("title", "market"), Rowles.LeanCorpus.Search.Occur.Should);
-        builder.Add(new TermQuery("body", "government"), Rowles.LeanCorpus.Search.Occur.Should);
-        builder.Add(new TermQuery("body", "market"), Rowles.LeanCorpus.Search.Occur.Should);
+        builder.Add(new TermQuery(FieldTitle, QueryTerm1), Rowles.LeanCorpus.Search.Occur.Should);
+        builder.Add(new TermQuery(FieldTitle, QueryTerm2), Rowles.LeanCorpus.Search.Occur.Should);
+        builder.Add(new TermQuery(FieldBody, QueryTerm1), Rowles.LeanCorpus.Search.Occur.Should);
+        builder.Add(new TermQuery(FieldBody, QueryTerm2), Rowles.LeanCorpus.Search.Occur.Should);
         return _leanSearcher!.Search(builder.Build(), TopN).TotalHits;
     }
 
@@ -104,10 +107,10 @@ public class CombinedFieldsQueryBenchmarks
     public int LuceneNet_BooleanQuery_MultiField()
     {
         var bq = new Lucene.Net.Search.BooleanQuery();
-        bq.Add(new LuceneTermQuery(new LuceneTerm("title", "government")), Occur.SHOULD);
-        bq.Add(new LuceneTermQuery(new LuceneTerm("title", "market")), Occur.SHOULD);
-        bq.Add(new LuceneTermQuery(new LuceneTerm("body", "government")), Occur.SHOULD);
-        bq.Add(new LuceneTermQuery(new LuceneTerm("body", "market")), Occur.SHOULD);
+        bq.Add(new LuceneTermQuery(new LuceneTerm(FieldTitle, QueryTerm1)), Occur.SHOULD);
+        bq.Add(new LuceneTermQuery(new LuceneTerm(FieldTitle, QueryTerm2)), Occur.SHOULD);
+        bq.Add(new LuceneTermQuery(new LuceneTerm(FieldBody, QueryTerm1)), Occur.SHOULD);
+        bq.Add(new LuceneTermQuery(new LuceneTerm(FieldBody, QueryTerm2)), Occur.SHOULD);
         return _luceneSearcher!.Search(bq, TopN).TotalHits;
     }
 
@@ -120,19 +123,14 @@ public class CombinedFieldsQueryBenchmarks
         using var writer = new Lucene.Net.Index.IndexWriter(
             _luceneDirectory,
             new Lucene.Net.Index.IndexWriterConfig(LuceneVersion.LUCENE_48, analyser));
-        for (int i = 0; i < documents.Length; i++)
+        AddDocuments(documents, (id, title, body) =>
         {
             var doc = new LuceneDocument();
-            doc.Add(new LuceneStringField("id",
-                i.ToString(System.Globalization.CultureInfo.InvariantCulture),
-                Lucene.Net.Documents.Field.Store.NO));
-            var body = documents[i];
-            var dot = body.IndexOf('.', StringComparison.Ordinal);
-            var title = dot > 0 && dot <= 120 ? body[..dot] : body[..Math.Min(80, body.Length)];
-            doc.Add(new LuceneTextField("title", title, Lucene.Net.Documents.Field.Store.NO));
-            doc.Add(new LuceneTextField("body", body, Lucene.Net.Documents.Field.Store.NO));
+            doc.Add(new LuceneStringField("id", id, Lucene.Net.Documents.Field.Store.NO));
+            doc.Add(new LuceneTextField(FieldTitle, title, Lucene.Net.Documents.Field.Store.NO));
+            doc.Add(new LuceneTextField(FieldBody, body, Lucene.Net.Documents.Field.Store.NO));
             writer.AddDocument(doc);
-        }
+        });
         writer.Commit();
         _luceneReader = LuceneDirectoryReader.Open(_luceneDirectory);
         _luceneSearcher = new LuceneIndexSearcher(_luceneReader);
@@ -146,19 +144,32 @@ public class CombinedFieldsQueryBenchmarks
         using var writer = new Rowles.LeanCorpus.Index.Indexer.IndexWriter(
             _leanDirectory,
             new Rowles.LeanCorpus.Index.Indexer.IndexWriterConfig { MaxBufferedDocs = 10_000, RamBufferSizeMB = 256 });
-        for (int i = 0; i < documents.Length; i++)
+        AddDocuments(documents, (id, title, body) =>
         {
             var doc = new LeanDocument();
-            doc.Add(new LeanStringField("id", i.ToString(System.Globalization.CultureInfo.InvariantCulture)));
-            // Extract a title from the first sentence
+            doc.Add(new LeanStringField("id", id));
+            doc.Add(new LeanTextField(FieldTitle, title));
+            doc.Add(new LeanTextField(FieldBody, body));
+            writer.AddDocument(doc);
+        });
+        writer.Commit();
+        _leanSearcher = new LeanIndexSearcher(_leanDirectory);
+    }
+
+    /// <summary>
+    /// Iterates <paramref name="documents"/>, extracts a title from the first
+    /// sentence, then calls <paramref name="add"/> for each document.
+    /// </summary>
+    private static void AddDocuments(
+        string[] documents,
+        Action<string, string, string> add)
+    {
+        for (int i = 0; i < documents.Length; i++)
+        {
             var body = documents[i];
             var dot = body.IndexOf('.', StringComparison.Ordinal);
             var title = dot > 0 && dot <= 120 ? body[..dot] : body[..Math.Min(80, body.Length)];
-            doc.Add(new LeanTextField("title", title));
-            doc.Add(new LeanTextField("body", body));
-            writer.AddDocument(doc);
+            add(i.ToString(System.Globalization.CultureInfo.InvariantCulture), title, body);
         }
-        writer.Commit();
-        _leanSearcher = new LeanIndexSearcher(_leanDirectory);
     }
 }
